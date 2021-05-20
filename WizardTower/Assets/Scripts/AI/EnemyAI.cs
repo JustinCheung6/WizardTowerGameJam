1b866465a9282c10c88b8b60f77fbf32fad3e182
Jessica Wei <jessicawei9@gmail.com>
2021-05-20 10:13:03 -0700

    + Chase implemented w/ jump (jump needs to be tested with actual player for any possible bugs) + Attack/Capture animation implemented, doesn't actually do anything yet

--- WizardTower/Assets/Scripts/AI/EnemyAI.cs
+++ WizardTower/Assets/Scripts/AI/EnemyAI.cs
@@ -10,6 +10,9 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] protected float idleTime = 2f;
     [SerializeField] protected float roamSpeed = 1f;
     [SerializeField] protected float chaseSpeed = 2f;
+    [SerializeField] protected float chaseJumpForce = 200f;
+    [SerializeField] protected float minCaptureRange = 2.5f;
+    [SerializeField] protected float captureExtraReactionTime = 0.5f;
     #endregion
 
     #region State Management Variables
@@ -31,6 +34,10 @@ public class EnemyAI : MonoBehaviour
 
     #region Chasing Variables
     [HideInInspector] public GameObject chasedObject;
+    #endregion
+
+    #region Capture Variables
+    protected float captureStartTime;
     #endregion
 
     #region Reference Variables
@@ -69,11 +76,14 @@ public class EnemyAI : MonoBehaviour
     protected void StateManager() {
         switch (state) {
             case State.Roaming:
-                MoveToPosition();
+                if (anim.GetBool("isAttacking"))
+                    anim.SetBool("isAttacking", false);
+                MoveToPosition(roamSpeed,0);
                 break;
             case State.Idling:
                 if (anim.GetBool("isRunning"))
                     anim.SetBool("isRunning", false);
+                
                 if (minRoamRange != 0 && maxRoamRange != 0) {
                     if (Time.time - idleStartTime >= idleTime) {
                         roamingPosition = GetRoamingPosition();
@@ -87,8 +97,25 @@ public class EnemyAI : MonoBehaviour
                     anim.SetBool("isRunning", true);
                 if (chasedObject != null)
                 {
-
+                    if (Mathf.Abs(chasedObject.transform.position.x - rb2d.position.x) > minCaptureRange)
+                    {
+                        roamingPosition = chasedObject.transform.position;
+                        MoveToPosition(chaseSpeed, chaseJumpForce);
+                    }
+                    else
+                    {
+                        state = State.Attacking;
+                    }
                 }
+                else {
+                    TriggerIdleState();
+                }
+                break;
+            case State.Attacking:
+                if (chasedObject.CompareTag("Player")) {
+                    anim.Play("attack");
+                }
+                TriggerIdleState();
                 break;
             default:
                 break;
@@ -99,20 +126,28 @@ public class EnemyAI : MonoBehaviour
         rb2d.MovePosition(rb2d.position + new Vector2(chaseSpeed, 0) * Time.fixedDeltaTime);
     }
 
-    protected void MoveToPosition() {
+    protected void MoveToPosition(float speedX, float jumpForce) {
         if ((roamingToRight && rb2d.position.x >= roamingPosition.x) || (!roamingToRight && rb2d.position.x <= roamingPosition.x)) {
             anim.SetBool("isRunning", false);
-            state = State.Idling;
-            idleStartTime = Time.time;
+            TriggerIdleState();
         }
         else
         {
             if (!anim.GetBool("isRunning"))
                 anim.SetBool("isRunning", true);
-            if (roamingToRight)
-                rb2d.MovePosition(rb2d.position + new Vector2(roamSpeed, 0) * Time.fixedDeltaTime);
-            else
-                rb2d.MovePosition(rb2d.position + new Vector2(roamSpeed*-1, 0) * Time.fixedDeltaTime);
+            if (roamingToRight && rb2d.velocity.y == 0)
+                //rb2d.MovePosition(rb2d.position + new Vector2(speedX, 0) * Time.fixedDeltaTime);
+                rb2d.velocity = new Vector2(speedX, 0);
+            else if(!roamingToRight && rb2d.velocity.y == 0)
+                //rb2d.MovePosition(rb2d.position + new Vector2(speedX * -1, 0) * Time.fixedDeltaTime);
+                rb2d.velocity = new Vector2(speedX * -1, 0);
+            if (chasedObject != null) {
+                Debug.Log(rb2d.velocity.y);
+                if(chasedObject.transform.position.y > rb2d.position.y && rb2d.velocity.y == 0)
+                {
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+                }
+            }
         }
     }
 
@@ -132,8 +167,7 @@ public class EnemyAI : MonoBehaviour
             roamingToRight = true;
         else
         {
-            state = State.Idling;
-            idleStartTime = Time.time;
+            TriggerIdleState();
         }
     }
 
@@ -148,5 +182,6 @@ public class EnemyAI : MonoBehaviour
     public void TriggerIdleState() {
         state = State.Idling;
         idleStartTime = Time.time;
+        
     }
 }
