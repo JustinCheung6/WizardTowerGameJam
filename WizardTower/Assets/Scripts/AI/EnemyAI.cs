11ad19135895a55440915289066b13c0a3f364b0
Jessica Wei <jessicawei9@gmail.com>
2021-05-23 21:41:14 -0700

    + added method that allowed for external forces on the AI

--- WizardTower/Assets/Scripts/AI/EnemyAI.cs
+++ WizardTower/Assets/Scripts/AI/EnemyAI.cs
@@ -321,6 +321,10 @@ public class EnemyAI : MonoBehaviour
         state = State.Stunned;
     }
 
+    public void ExternalForce(float xVelocity, float yVelocity = 0f) {
+        MoveToPosition(xVelocity, yVelocity);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !CheckForStun()) {
