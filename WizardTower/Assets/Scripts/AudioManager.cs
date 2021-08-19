3f4ccf501911da5fe83fe2d4ff0e8c4e3763519c
Jessica Wei <jessicawei9@gmail.com>
2021-08-19 14:22:44 -0700

    + need to commit to pull

---
+++ WizardTower/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{ 
+    [SerializeField] private List<AudioClip> bmgs;
+    [SerializeField] private List<AudioClip> sfx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
